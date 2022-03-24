018e567a63de0bb7feaade3858846607b6a6a12b
Matthew Jakeman <mjakeman26@outlook.co.nz>
2022-03-24 14:34:54 +1300

    main: Add token parsing logic

--- src/main.c
+++ src/main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define bool int
 #define FALSE 0
@@ -9,17 +14,41 @@ void print_prompt ()
     printf ("# ");
 }
 
-char **parse_input (int *n_tokens)
+char **parse_input (char **command, int *n_tokens)
 {
     char *buffer = NULL;
     size_t buffer_size = 0;
 
-    size_t length = getline (&buffer, &buffer_size, stdin);
+    getline (&buffer, &buffer_size, stdin);
 
-    printf ("length: %ld / capacity: %ld \n", length, buffer_size);
+    // a string with lots of spaces in it
+    //  -> an array of strings with no whitespace
+    //  -> string.split(my_str, " \t")
 
-    *n_tokens = 0;
-    return NULL;
+    char *command_token;
+
+    command_token = strtok (buffer, " \t");
+    if (command_token == NULL)
+    {
+        *n_tokens = 0;
+        *command = NULL;
+        return NULL;
+    }
+
+    *command = command_token;
+
+    char **tokens = malloc (sizeof(char*) * 100);
+    char *cur_token = command_token;
+
+    int index = 0;
+    while (cur_token)
+    {
+        cur_token = strtok (NULL, " \t");
+        tokens[index++] = cur_token;
+    }
+
+    *n_tokens = index+1;
+    return tokens;
 }
 
 int main ()
@@ -31,8 +60,29 @@ int main ()
     while (running)
     {
         int n_tokens;
+        char **tokens;
+        char *command;
 
         print_prompt ();
-        parse_input (&n_tokens);
+        tokens = parse_input (&command, &n_tokens);
+
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            // we are the child
+            int result = execvp (command, tokens);
+
+            if (result == -1)
+            {
+                printf ("No process '%s' \n", command);
+                return 1;
+            }
+
+            return 0;
+        }
+
+        waitpid (pid, NULL, 0);
+
+        free (tokens);
     }
 }
