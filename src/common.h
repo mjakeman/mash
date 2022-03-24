c5e21685dec410fad520af348f6ef08b662abbdb
Matthew Jakeman <mjakeman26@outlook.co.nz>
2022-03-25 09:32:56 +1300

    common: Add ANSI Colour Codes (and attribution)

--- src/common.h
+++ src/common.h
@@ -11,3 +11,15 @@
 #define TRUE 1
 
 #define BUFFER_SIZE 500
+
+// Colour Output
+// This uses ANSI escape codes to colour the terminal output. On platforms
+// where colour output is not available, this should simply have no effect.
+// All credit to: https://stackoverflow.com/questions/3219393/stdlib-and-colored-output-in-c
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
